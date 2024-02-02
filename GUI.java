f3a1175961c2ee441651239ec232e9f89b33c529
UpRelish <109433092+UpRelish@users.noreply.github.com>
2024-02-02 10:24:08 -0500

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,3 +1,24 @@
 class GUI {
+    import javax.swing.*;
+    import java.awt.event.*;
+    import java.awt.*;
+    import java.util.Random;
+    private String state;
+
+    public void run (){
+        switch (state){
+            case MAIN:
+                //draw screen
+                break;
+            case DIFFICULTY:
+                //draw screen
+                break;
+            case PLAY:
+                //draw screen
+                break;
+        }
+    }
+
     
+
 }
