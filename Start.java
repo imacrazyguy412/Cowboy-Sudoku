575089978910331ff4fc59f80c7f4a9a3007cf75
UpRelish <109433092+UpRelish@users.noreply.github.com>
2024-02-05 09:59:15 -0500

    Update Start.java

--- Start.java
+++ Start.java
@@ -5,7 +5,24 @@ import java.util.Random;
 
 class Start extends JPanel {
   private JLabel title, cowboy;
-  private JButton playGame, continueGame;
+  private JButton playGame, savedGame;
+
+  public Start() {
+		//Sets the content pane
+		this.setBorder(BorderFactory.createEmptyBorder (10, 10, 10, 10));
+		this.setBackground(Color.white);
+		this.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		
+		//Create and add both labels
+		title = new JLabel("Cowboy Sudoku");
+		cowboy = new JLabel(new ImageIcon ("src/die1.gif"));
+		
+		//Create and add button
+		playGame = new JButton("Play New Game");
+    savedGame = new JButton ("Continue Saved Game");
+				
+				
+	}
   
   
   
