8e4cf6c0c004bd4c72a78f3fe33bf86ac5fb35c9
UpRelish <109433092+UpRelish@users.noreply.github.com>
2024-02-05 10:37:23 -0500

    Update Start.java

--- Start.java
+++ Start.java
@@ -3,25 +3,40 @@ import java.awt.event.*;
 import java.awt.*;
 import java.util.Random;
 
-class Start extends JPanel {
-  private JLabel title, cowboy;
+class Start extends JPanel{
+  private JLabel title, logo;
   private JButton playGame, savedGame;
+ 
 
   public Start() {
 	//Sets the content pane
-	this.setBorder(BorderFactory.createEmptyBorder (10, 10, 10, 10));
-	this.setBackground(Color.white);
-	this.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+	this.setBackground(new Color (229,229,229,100));
+	this.setLayout(new GridBagLayout());
+	GridBagConstraints c = new GridBagConstraints();
 		
-	//Create and add both labels
+	//Create both labels
 	title = new JLabel("Cowboy Sudoku");
-	cowboy = new JLabel(new ImageIcon (""));
+	logo = new JLabel(new ImageIcon ("GUI/logo.png"));
+	//set constraints and add title
+	c.gridx = 1;
+	c.gridy = 0;
+	this.add(title, c);
+	//set constraints and add logo;
+	c.gridx = 1;
+	c.gridy = 2;
+	this.add(logo, c);
 		
-	//Create and add button
+	//Create buttons
 	playGame = new JButton("Play New Game");
    	savedGame = new JButton ("Continue Saved Game");
-				
-				
+	//set constraints and add playGame;
+	c.gridx = 0;
+	c.gridy = 1;
+	this.add (playGame, c);
+	//set constraints and add savedGame;
+	c.gridx = 2;
+	c.gridy = 1;
+	this.add (savedGame, c);
 	}
   
 }
