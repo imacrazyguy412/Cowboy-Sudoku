4d99981c5843bb02f9c43e223d30094ce8f59d98
UpRelish <109433092+UpRelish@users.noreply.github.com>
2024-02-06 10:05:08 -0500

    Update Difficulty.java

--- Difficulty.java
+++ Difficulty.java
@@ -5,40 +5,41 @@ import java.awt.*;
 import java.util.Random;
 
 class Difficulty extends JPanel{
-  private JLabel header;
-  private JButton easy, medium, hard, back, start, mistakeToggle;
+	private JLabel header;
+	private JButton easy, medium, hard, back, start, mistakeToggle;
  
-
-  public Start() {
-	  //Sets the content pane
-	  this.setBackground(new Color (229,229,229,100));
-  	this.setLayout(new GridBagLayout());
-	  GridBagConstraints c = new GridBagConstraints();
+	public Start() {
+		//Sets the content pane
+		this.setBackground(new Color (229,229,229,100));
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
 		
-	  //Create label
-	  title = new JLabel("Difficulty Selection");
-	  logo = new JLabel(new ImageIcon ("GUI/logo.png"));
-	  //set constraints and add title
-	  c.gridx = 1;
-	  c.gridy = 0;
-	  this.add(title, c);
-	  //set constraints and add logo;
-  	c.gridx = 1;
-	  c.gridy = 2;
-	  this.add(logo, c);
+		//Create header label
+		header = new JLabel("Difficulty Selection");
+		//set constraints and add title
+		c.gridx = 1;
+		c.gridy = 0;
+		this.add(header, c);
 		
-  	//Create buttons
-  	playGame = new JButton("Play New Game");
-  	playGame.setActionCommand()
-   	savedGame = new JButton ("Continue Saved Game");
-  	//set constraints and add playGame;
-  	c.gridx = 0;
-  	c.gridy = 1;
-	  this.add (playGame, c);
-  	//set constraints and add savedGame;
-  	c.gridx = 2;
-	  c.gridy = 1;
-  	this.add (savedGame, c);
+		//Create buttons
+		easy = new JButton("Easy");
+		medium = new JButton ("Medium");
+		hard = new JButton ("Hard");
+		back = new JButton ("Back");
+		start = new JButton ("Start");
+		mistakeToggle = new JButton ("Toggle Mistakes Limiter");
+		
+		//set constraints and add mistakeToggle;
+		c.gridx = 0;
+		c.gridy = 0;
+		this.add (mistakeToggle, c);
+		//set constraints and add easy;
+		c.gridx = 1;
+		c.gridy = 1;
+		this.add (easy, c);
+		//set constraints and add medium;
+		c.gridx = 1;
+		c.gridy = 1;
+		this.add (easy, c);
 	}
-  
 }
