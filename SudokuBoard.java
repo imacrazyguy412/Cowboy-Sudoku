c894b96475a72b31eac990191f0d4d5836173e01
imacrazyguy412 <110923269+imacrazyguy412@users.noreply.github.com>
2024-02-14 10:48:56 -0500

    Update and rename ButtonPane.java to SudokuBoard.java

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -8,7 +8,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class ButtonPane extends JPanel {
+public class SudokuBoard extends JPanel {
 
    private JButton[][] buttons;
 
@@ -16,7 +16,7 @@ public class ButtonPane extends JPanel {
     //just use a png background and make wiindow non resisable
     //add scaling/actual gui component later
 
-   public ButtonPane(int row, int col) {
+   public SudokuBoard(int row, int col) {
       super(new GridLayout(row, col));
       buttons = new JButton[row][col];
       for (int i = 0; i < buttons.length; i++) {
@@ -25,6 +25,7 @@ public class ButtonPane extends JPanel {
             final int curCol = j;
             buttons[i][j] = new JButton("");
             buttons[i][j].addKeyListener(enter);
+            buttons[i][j].addKeyListener(number);
             buttons[i][j].setBackground(Color.white);
             buttons[i][j].addKeyListener(new KeyAdapter() {
                @Override
@@ -34,26 +35,26 @@ public class ButtonPane extends JPanel {
                      if (curRow > 0)
                     	buttons[curRow][curCol].setBackground(Color.white);
                         buttons[curRow - 1][curCol].requestFocus();
-                     	buttons[curRow - 1][curCol].setBackground(Color.blue);
+                     	buttons[curRow - 1][curCol].setBackground(new Color(173,216,230));
                     // buttons[curRow - 1][curCol].se
                      break;
                   case KeyEvent.VK_DOWN:
                      if (curRow < buttons.length - 1)
                     	buttons[curRow][curCol].setBackground(Color.white);
                         buttons[curRow + 1][curCol].requestFocus();
-                     	buttons[curRow + 1][curCol].setBackground(Color.BLUE);
+                     	buttons[curRow + 1][curCol].setBackground(new Color(173,216,230));
                      break;
                   case KeyEvent.VK_LEFT:
                      if (curCol > 0)
                     	buttons[curRow][curCol].setBackground(Color.white);
                         buttons[curRow][curCol - 1].requestFocus();
-                     	buttons[curRow][curCol - 1].setBackground(Color.BLUE);
+                     	buttons[curRow][curCol - 1].setBackground(new Color(173,216,230));
                      break;
                   case KeyEvent.VK_RIGHT:
                      if (curCol < buttons[curRow].length - 1)
                     	buttons[curRow][curCol].setBackground(Color.white);
                         buttons[curRow][curCol + 1].requestFocus();
-                        buttons[curRow][curCol + 1].setBackground(Color.BLUE);
+                        buttons[curRow][curCol + 1].setBackground(new Color(173,216,230));
                      break;
                   default:
                      break;
@@ -64,6 +65,17 @@ public class ButtonPane extends JPanel {
          }
       }
    }
+   
+   void setNote(JButton button, int note) {
+	   
+	   if(button.getText().contains(note + "")) {
+		   System.out.println(button.getText());
+		   System.out.println("removed" + note);
+		   button.setText(button.getText().substring(0, button.getText().indexOf(note + "")) + button.getText().substring(button.getText().indexOf(note + "")+1));
+	   } else {
+		   button.setText(button.getText() + note);
+	   }
+   }
 
    private KeyListener enter = new KeyAdapter() {
       @Override
@@ -73,11 +85,47 @@ public class ButtonPane extends JPanel {
          }
       }
    };
+   private KeyListener number = new KeyAdapter() {
+	      @Override
+	      public void keyTyped(KeyEvent e) {
+	    	  switch(e.getKeyChar()) {
+	    	  case KeyEvent.VK_1:
+	    		  setNote((JButton)( e.getComponent()), 1);
+	    		  break;
+	    	  case KeyEvent.VK_2:
+	    		  setNote((JButton)( e.getComponent()), 2);
+	    		  break;
+	    	  case KeyEvent.VK_3:
+	    		  setNote((JButton)( e.getComponent()), 3);
+break;
+	    	  case KeyEvent.VK_4:
+	    		  setNote((JButton)( e.getComponent()), 4);
+break;
+	    	  case KeyEvent.VK_5:
+	    		  setNote((JButton)( e.getComponent()), 5);
+break;
+	    	  case KeyEvent.VK_6:
+	    		  setNote((JButton)( e.getComponent()), 6);
+break;
+	    	  case KeyEvent.VK_7:
+	    		  setNote((JButton)( e.getComponent()), 7);
+break;
+	    	  case KeyEvent.VK_8:
+	    		  setNote((JButton)( e.getComponent()), 8);
+break;
+	    	  case KeyEvent.VK_9:
+	    		  setNote((JButton)( e.getComponent()), 9);
+break;
+	    	  }
+	    	  
+	         
+	      }
+	   };
 
    public static void main(String[] args) {
       JFrame f = new JFrame();
       f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      f.add(new ButtonPane(9, 9));
+      f.add(new SudokuBoard(9, 9));
       //f.pack();
       f.setSize(400,400);
       f.setVisible(true);
