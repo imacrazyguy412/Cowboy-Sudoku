55b156c157c83fb4d7598f1b815e668ee78a9129
hasake <powerfulaidan@gmail.com>
2024-02-14 10:44:30 -0500

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.util.Random;
 class GUI {
     private String state;
+    static JFrame frame;
 
     public void run (){
         switch (state){
@@ -24,10 +25,17 @@ class GUI {
      * have a frame and then add startscreen to it here
      */
     public static void main(String[] args) {
+    	frame = new JFrame();
     	StartScreen start = new StartScreen();
+    	Difficulty diff = new Difficulty();
+    	start.addComponentsToPane(start);
     	start.createAndShowGUI();
+    	frame.add(start);
+    	diff.addToPane(diff);
+    	
+    	frame.setVisible(true);
+    	
     }
 
-    
 
 }
