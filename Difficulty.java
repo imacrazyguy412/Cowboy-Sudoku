e2361404b7f331c4569757273ff485715719fd07
hasake <powerfulaidan@gmail.com>
2024-02-14 10:42:22 -0500

    Update Difficulty.java

--- Difficulty.java
+++ Difficulty.java
@@ -22,19 +22,19 @@ import java.util.Random;
 class Difficulty {
     private  JLabel header;
     private  JButton easy, medium, hard, back, start, mistakeToggle;
-    static JFrame Diffframe;
+    static JPanel Difframe;
     //private  StartScreen newStartScreen;
     //easy: 1, medium: 2, hard: 3
      int gameDiff;
      
      public Difficulty() {
-    	    Diffframe = new JFrame("Cowboy Sudoku");
+    	 Difframe = new JPanel();
      }
 
-    public  void addToPane (Container pane) {
+    public  void addToPane(Container Difframe) {
         //Sets the content pane
-        pane.setBackground(new Color (229,229,229,100));
-        pane.setLayout(new GridBagLayout());
+    	Difframe.setBackground(new Color (229,229,229,100));
+    	Difframe.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
 
         //Create header label
@@ -48,7 +48,7 @@ class Difficulty {
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 1;
         c.gridy = 0;
-        pane.add(header, c);
+        Difframe.add(header, c);
 
         //Create buttons
         easy = new JButton("Easy");
@@ -61,7 +61,7 @@ class Difficulty {
         //set constraints and add easy;
         c.gridx = 1;
         c.gridy = 1;
-        pane.add (easy, c);
+        Difframe.add (easy, c);
 
         medium = new JButton ("Medium");
         medium.setOpaque(true);
@@ -73,7 +73,7 @@ class Difficulty {
         //set constraints and add medium;
         c.gridx = 1;
         c.gridy = 2;
-        pane.add (medium, c);
+        Difframe.add (medium, c);
 
         hard = new JButton ("Hard");
         hard.setOpaque(true);
@@ -85,22 +85,22 @@ class Difficulty {
         //set constraints and add hard;
         c.gridx = 1;
         c.gridy = 3;
-        pane.add (hard, c);
+        Difframe.add (hard, c);
 
         back = new JButton ("Back");
         back.setOpaque(true);
         back.setBackground(new Color (36, 44, 61));
         back.setForeground (Color.white);
         back.addActionListener(event -> {
-            Diffframe.dispose();
-            newStartScreen.secondShow();
+        	Difframe.setVisible(false);
+            //newStartScreen.secondShow();
             System.out.println("frame is closed");
         });
 
         //set constraints and add back;
         c.gridx = 0;
         c.gridy = 4;
-        pane.add (back, c);
+        Difframe.add (back, c);
 
 
         start = new JButton ("Start");
@@ -110,7 +110,7 @@ class Difficulty {
         //set constraints and add start;
         c.gridx = 2;
         c.gridy = 4;
-        pane.add (start, c);
+        Difframe.add (start, c);
 
 
         mistakeToggle = new JButton ("Toggle Mistakes");
@@ -121,9 +121,13 @@ class Difficulty {
         //set constraints and add mistakeToggle;
         c.gridx = 0;
         c.gridy = 0;
-        pane.add(mistakeToggle, c);
+        Difframe.add(mistakeToggle, c);
 
     }
+    public JComponent getPanel() {
+		// TODO Auto-generated method stub
+		return Difframe;
+	}
     /**
      * Create the GUI and show it.  For thread safety,
      * this method should be invoked from the
@@ -131,22 +135,15 @@ class Difficulty {
      */
      void createAndShowGUI() {
         //Create and set up the window.
-        Diffframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	 //Difframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Set up the content pane.
-        addToPane(Diffframe.getContentPane());
+        //addToPane(Difframe.getContentPane());
 
         //Display the window.
-        Diffframe.pack();
-        Diffframe.setVisible(true);
-        Diffframe.setResizable(false);
-    }
-     void secondShow(){
-        Diffframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Diffframe.setSize(900,1200);
-        Diffframe.setResizable(false);
-        Diffframe.pack();
-        Diffframe.setVisible(true);
+        //Difframe.pack();
+        Difframe.setVisible(true);
+        //Difframe.setResizable(false);
     }
 
     public static void main(String[] args) {
@@ -159,4 +156,5 @@ class Difficulty {
             }
         });
     }
+
 }
