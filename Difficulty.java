be0956452fffbe56784d30efee7160b0ca13dd64
imacrazyguy412 <110923269+imacrazyguy412@users.noreply.github.com>
2024-02-16 10:32:49 -0500

    Update Difficulty.java

--- Difficulty.java
+++ Difficulty.java
@@ -1,4 +1,3 @@
-
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
@@ -19,29 +18,29 @@ import java.util.Random;
 	c.weighty = 0.0;
 */
 
-class Difficulty extends JPanel{
-    private  JLabel header;
-    private  JButton easy, medium, hard, back, start, mistakeToggle;
-    public  static JPanel Difframe;
-    
+class Difficulty extends JPanel {
+    private JLabel header;
+    private JButton easy, medium, hard, back, start, mistakeToggle;
+    public static JPanel Difframe;
+
     //private  StartScreen newStartScreen;
     //easy: 1, medium: 2, hard: 3
-     int gameDiff;
-     
-     public Difficulty() {
-    	 Difframe = new JPanel();
-     }
+    int gameDiff;
 
-    public  void addComponentsToPane(Container Difframe) {
+    public Difficulty() {
+        Difframe = new JPanel();
+    }
+
+    public void addComponentsToPane(Container Difframe) {
         //Sets the content pane
-    	Difframe.setBackground(new Color (229,229,229));
-    	Difframe.setLayout(new GridBagLayout());
+        Difframe.setBackground(new Color(229, 229, 229));
+        Difframe.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
 
         //Create header label
         header = new JLabel("Difficulty Selection");
         header.setOpaque(true);
-        header.setBackground(new Color (36, 44, 61));
+        header.setBackground(new Color(36, 44, 61));
         header.setForeground(Color.white);
 
 
@@ -54,75 +53,75 @@ class Difficulty extends JPanel{
         //Create buttons
         easy = new JButton("Easy");
         easy.setOpaque(true);
-        easy.setBackground(new Color (207, 176, 100));
-        easy.setForeground (Color.black);
-        easy.addActionListener(event -> {
+        easy.setBackground(new Color(207, 176, 100));
+        easy.setForeground(Color.black);
+        easy.addActionListener(event - > {
             gameDiff = 1;
-            
+
         });
         //set constraints and add easy;
         c.gridx = 1;
         c.gridy = 1;
-        Difframe.add (easy, c);
+        Difframe.add(easy, c);
 
-        medium = new JButton ("Medium");
+        medium = new JButton("Medium");
         medium.setOpaque(true);
-        medium.setBackground(new Color (36, 44, 61));
-        medium.setForeground (Color.white);
-        medium.addActionListener(event -> {
+        medium.setBackground(new Color(36, 44, 61));
+        medium.setForeground(Color.white);
+        medium.addActionListener(event - > {
             gameDiff = 2;
         });
         //set constraints and add medium;
         c.gridx = 1;
         c.gridy = 2;
-        Difframe.add (medium, c);
+        Difframe.add(medium, c);
 
-        hard = new JButton ("Hard");
+        hard = new JButton("Hard");
         hard.setOpaque(true);
-        hard.setBackground(new Color (207, 176, 100));
-        hard.setForeground (Color.black);
-        hard.addActionListener(event -> {
+        hard.setBackground(new Color(207, 176, 100));
+        hard.setForeground(Color.black);
+        hard.addActionListener(event - > {
             gameDiff = 3;
-                });
+        });
         //set constraints and add hard;
         c.gridx = 1;
         c.gridy = 3;
-        Difframe.add (hard, c);
+        Difframe.add(hard, c);
 
-        back = new JButton ("Back");
+        back = new JButton("Back");
         back.setOpaque(true);
-        back.setBackground(new Color (36, 44, 61));
-        back.setForeground (Color.white);
-        back.addActionListener(event -> {
-        	GUI.cowboyFrame.setContentPane(GUI.startM);
+        back.setBackground(new Color(36, 44, 61));
+        back.setForeground(Color.white);
+        back.addActionListener(event - > {
+            GUI.cowboyFrame.setContentPane(GUI.startM);
             System.out.println("frame is closed");
         });
 
         //set constraints and add back;
         c.gridx = 0;
         c.gridy = 4;
-        Difframe.add (back, c);
+        Difframe.add(back, c);
 
 
-        start = new JButton ("Start");
+        start = new JButton("Start");
 
-        start.setBackground(new Color (36, 44, 61));
-        start.setForeground (Color.white);
-        start.addActionListener(event -> {
-        	if (gameDiff == 1)
+        start.setBackground(new Color(36, 44, 61));
+        start.setForeground(Color.white);
+        start.addActionListener(event - > {
+            if (gameDiff == 1)
                 GUI.cowboyFrame.setContentPane(GUI.gameME);
-        	
+
         });
         //set constraints and add start;
         c.gridx = 2;
         c.gridy = 4;
-        Difframe.add (start, c);
+        Difframe.add(start, c);
 
 
-        mistakeToggle = new JButton ("Toggle Mistakes");
+        mistakeToggle = new JButton("Toggle Mistakes");
         mistakeToggle.setOpaque(true);
-        mistakeToggle.setBackground(new Color (36, 44, 61));
-        mistakeToggle.setForeground (Color.white);
+        mistakeToggle.setBackground(new Color(36, 44, 61));
+        mistakeToggle.setForeground(Color.white);
 
         //set constraints and add mistakeToggle;
         c.gridx = 0;
@@ -131,17 +130,17 @@ class Difficulty extends JPanel{
 
     }
     public JComponent getPanel() {
-		// TODO Auto-generated method stub
-		return Difframe;
-	}
+        // TODO Auto-generated method stub
+        return Difframe;
+    }
     /**
      * Create the GUI and show it.  For thread safety,
      * this method should be invoked from the
      * event-dispatching thread.
      */
-     void createAndShowGUI() {
+    void createAndShowGUI() {
         //Create and set up the window.
-    	 //Difframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Difframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Set up the content pane.
         //addToPane(Difframe.getContentPane());
@@ -155,19 +154,17 @@ class Difficulty extends JPanel{
     public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
-    	Difficulty diff = new Difficulty();
+        Difficulty diff = new Difficulty();
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 diff.createAndShowGUI();
             }
         });
     }
-    
+
     public void backToStart() {
-        	Difframe.setVisible(false);
-            //newStartScreen.secondShow();
-            System.out.println("frame is closed");
-        }
+        Difframe.setVisible(false);
+        //newStartScreen.secondShow();
+        System.out.println("frame is closed");
     }
-
-
+}
