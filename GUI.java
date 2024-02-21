3f115fdac7339638b4a40433717e62cc77f7839f
UpRelish <109433092+UpRelish@users.noreply.github.com>
2024-02-21 10:13:22 -0500

    this is still nevan and im an asshole

--- GUI.java
+++ GUI.java
@@ -6,31 +6,31 @@ class GUI {
     public static JFrame cowboyFrame = new JFrame();
     public static StartScreen startM = new StartScreen();
     public static Difficulty diffM = new Difficulty();
-    public static Easy gameME = new Easy();
+    public static Game gameM = new Game();
 
-    public void run() {
+    public void run (){
         startM.addComponentsToPane(startM);
         startM.createAndShowGUI();
         diffM.addComponentsToPane(diffM);
         diffM.createAndShowGUI();
-        gameME.addComponentsToPane(gameME);
-        gameME.createAndShowGUI();
-
+        gameM.addComponentsToPane(gameM);
+        gameM.createAndShowGUI();
+        
         cowboyFrame.setContentPane(startM);
-
+        
     }
     /*
      * make startscreen a panel
      * have a frame and then add startscreen to it here
      */
     public static void main(String[] args) {
-        cowboyFrame.setVisible(true);
-        GUI sudoku = new GUI();
-        sudoku.run();
-
-
+    	cowboyFrame.setVisible(true);
+    	GUI sudoku = new GUI ();
+    	sudoku.run();
+    	
+    	
     }
-
+    
 
 
 }
