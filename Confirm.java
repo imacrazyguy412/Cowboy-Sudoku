28ecd16348b51f330aa35a903a7d824683c99f50
UpRelish <109433092+UpRelish@users.noreply.github.com>
2024-02-26 10:32:03 -0500

    Update Confirm.java

--- Confirm.java
+++ Confirm.java
@@ -22,7 +22,6 @@ import java.util.Random;
 class Confirm extends JPanel {
 	private static JLabel header;
 	private static JButton back, conf, cancel;
-	static JFrame frame = new JFrame("Cowboy Sudoku");
  
 	public static void addToPane (Container pane, int button) {
 		//Sets the content pane
@@ -57,7 +56,11 @@ class Confirm extends JPanel {
         			//solve
         			break;
         		case 3:
-        			//new game
+        			GUI.gameM = new Game();
+        			GUI.gameM.createAndShowGUI();
+                	GUI.cowboyFrame.setContentPane(GUI.diffM);
+                	GUI.cowboyFrame.repaint();
+                	GUI.cowboyFrame.revalidate();
         			break;
         	}
         });
@@ -84,25 +87,7 @@ class Confirm extends JPanel {
  	* this method should be invoked from the
   	* event-dispatching thread.
 */
-    private static void createAndShowGUI() {
-        //Create and set up the window.
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Set up the content pane.
-        addToPane(frame.getContentPane(), 1);
 
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-    }
+    
 }
