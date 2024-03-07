1909ad93ed2f3c464028d0da36c2ba4f54be1342
UpRelish <109433092+UpRelish@users.noreply.github.com>
2024-03-07 10:07:45 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -273,8 +273,8 @@ class Game extends JPanel{
 		return pane;
 	}
     
-    public void updateTimer() {
-    	
+    public void setMistakes(int mist) {
+    	mistakes = mist;
     }
     
     public void updateMistakes() {
